3ef52e78b53a04706c61ea85914d8028ba80b83d
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-22 14:40:22 -0700

    add working

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -5,30 +5,40 @@ const initialState = {
     title: '',
     director: '',
     metascore: '',
-    stars: []
+    stars: ['']
 }
 
 export default function AddMovie(props) {
     const [movie, setMovie] = useState(initialState)
 
-    // useEffect(() => {
-    //     axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
-    //     .then(res => setMovie(res.data))
-    //     .catch(err => console.log(err))
-    // }, [props.match.params.id])
-
     const handleChange = e => {
         setMovie({...movie, [e.target.name]: e.target.value})
     }
 
+    const starsChangeHandler = (idx, e) => {
+        const updatedStars = [...movie.stars]
+        updatedStars[idx] = e.target.value
+        setMovie({
+        ...movie,
+        stars: updatedStars
+        })
+    }
+
     const handleSubmit = e => {
         e.preventDefault()
-        axios.post(`http://localhost:5000/api/movies}`, movie)
+        axios.post(`http://localhost:5000/api/movies`, movie)
         .then(res => {
             setMovie(initialState)
-            props.history.push(`/movies/${props.match.params.id}`)
+            props.history.push(`/`)
         })
         .catch(err => console.log(err))
+    }
+
+    const addActors = e => {
+        e.preventDefault()
+        setMovie({...movie,
+            stars: [...movie.stars, '']
+        })
     }
     return (
         <div>
@@ -37,8 +47,9 @@ export default function AddMovie(props) {
                 <input type='text' name='title' value={movie.title} onChange={handleChange} placeholder='Title'></input>
                 <input type='text' name='director' value={movie.director} onChange={handleChange} placeholder='Director'></input>
                 <input type='text' name='metascore' value={movie.metascore} onChange={handleChange} placeholder='Metascore'></input>
-                <input type='text' name='stars' value={movie.stars} onChange={handleChange} placeholder='Actors'></input>
-                <button>Update Movie</button>
+                {movie.stars.map((star, idx) => <input key={idx} type='text' name='stars' value={star} onChange={(e) => starsChangeHandler(idx, e)} placeholder='Actors'></input>)}
+                <button onClick={addActors}>Add Actors Input</button>
+                <button>Add Movie</button>
             </form>
         </div>
     )
