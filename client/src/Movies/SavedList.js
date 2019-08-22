4d6e8af938b3201cacfb49e418dea183cbfb5ba2
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-22 14:43:01 -0700

    fixed warnings

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+ 
   render() {
     return (
       <div className="saved-list">
